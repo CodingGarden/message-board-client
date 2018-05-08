1e768be90282f4d9f377bd474eb4a8d2232cb1fc
CJ R <cj@null.computer>
2018-05-08 16:52:08 -0600

    :sparkles: Working app!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,126 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button
+      @click="showMessageForm = !showMessageForm"
+      type="button"
+      class="btn btn-info mt-3 mb-3">Toggle Message Form</button>
+    <form v-if="showMessageForm" @submit.prevent="addMessage" class="mb-3">
+      <div v-if="error" class="alert alert-dismissible alert-warning">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <h4 class="alert-heading">Error!</h4>
+        <p class="mb-0">{{error}}</p>
+      </div>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input
+          v-model="message.username"
+          type="text"
+          class="form-control"
+          id="username" required>
+      </div>
+      <div class="form-group">
+        <label for="subject">Subject</label>
+        <input
+          v-model="message.subject"
+          type="text"
+          class="form-control"
+          id="subject"
+          placeholder="Enter a subject" required>
+      </div>
+      <div class="form-group">
+        <label for="message">Message</label>
+        <textarea
+          v-model="message.message"
+          class="form-control"
+          id="message"
+          rows="3"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="imageURL">Image URL</label>
+        <input
+          v-model="message.imageURL"
+          type="url"
+          class="form-control"
+          id="imageURL"
+          placeholder="Put an image URL dummy">
+      </div>
+      <button type="submit" class="btn btn-primary">Add Message</button>
+    </form>
+    <div class="list-unstyled" v-for="message in reversedMessages" :key="message._id">
+      <li class="media">
+        <img v-if="message.imageURL" class="mr-3" :src="message.imageURL" :alt="message.subject">
+        <div class="media-body">
+          <h4 class="mt-0 mb-1">{{message.username}}</h4>
+          <h5 class="mt-0 mb-1">{{message.subject}}</h5>
+          {{message.message}}
+          <br />
+          <small>{{message.created}}</small>
+        </div>
+      </li>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+const API_URL = 'https://full-stack-message-board.herokuapp.com/messages';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data: () => ({
+    showMessageForm: false,
+    error: '',
+    messages: [],
+    message: {
+      username: 'Anonymous',
+      subject: '',
+      message: '',
+      imageURL: '',
+    },
+  }),
+  computed: {
+    reversedMessages() {
+      return this.messages.slice().reverse();
+    },
+  },
+  mounted() {
+    fetch(API_URL).then(response => response.json()).then((result) => {
+      this.messages = result;
+    });
+  },
+  methods: {
+    addMessage() {
+      console.log(this.message);
+      fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(this.message),
+        headers: {
+          'content-type': 'application/json',
+        },
+      }).then(response => response.json()).then((result) => {
+        if (result.details) {
+          // there was an error...
+          const error = result.details.map(detail => detail.message).join('. ');
+          this.error = error;
+        } else {
+          this.error = '';
+          this.showMessageForm = false;
+          this.messages.push(result);
+        }
+      });
+    },
   },
 };
 </script>
+
+<style>
+hr {
+  border-top: 1px solid white;
+}
+
+img {
+  max-width: 300px;
+  height: auto;
+}
+</style>
+
